4fae38e276a2630804379916e548f0bdc0240706
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2021-08-24 23:43:49 +0800

    minor changes and unfinished code on BuildViewHolder and BuildAdapter

--- app/src/main/java/com/mobdeve/group30/genshinbuilds/BuildAdapter.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/BuildAdapter.java
@@ -1,4 +1,43 @@
 package com.mobdeve.group30.genshinbuilds;
 
-public class BuildAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class BuildAdapter extends RecyclerView.Adapter<BuildViewHolder> {
+
+    private ArrayList<Build> dataBuilds;
+
+    public BuildAdapter(ArrayList<Build> dataBuilds) { this.dataBuilds = dataBuilds; }
+
+    @NonNull
+    @NotNull
+    @Override
+    public BuildViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.item_build, parent, false);
+        BuildViewHolder buildViewHolder = new BuildViewHolder(itemView);
+
+        return buildViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull BuildViewHolder holder, int position) {
+        Build currentBuild = this.dataBuilds.get(position);
+
+        // call functions from Build.java
+        // currentBuild.set
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.dataBuilds.size();
+    }
 }
--- app/src/main/java/com/mobdeve/group30/genshinbuilds/BuildViewHolder.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/BuildViewHolder.java
@@ -1,4 +1,14 @@
 package com.mobdeve.group30.genshinbuilds;
 
-public class BuildViewHolder {
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BuildViewHolder extends RecyclerView.ViewHolder {
+    public BuildViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+    }
 }
