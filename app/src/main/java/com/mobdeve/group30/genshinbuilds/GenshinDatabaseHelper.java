9de1b513858b4fff26ae5f955e3aeac8e1b2ab43
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2021-08-25 23:08:26 +0800

    Insert user functionality is 80% done

--- app/src/main/java/com/mobdeve/group30/genshinbuilds/GenshinDatabaseHelper.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/GenshinDatabaseHelper.java
@@ -1,4 +1,87 @@
 package com.mobdeve.group30.genshinbuilds;
 
-public class GenshinDatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.sql.Date;
+
+public class GenshinDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "GenshinBuilds.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String USER_TABLE_NAME = "users";
+    private static final String USER_COLUMN_ID = "_id";
+    private static final String USER_COLUMN_USERNAME = "username";
+    private static final String USER_COLUMN_PASSWORD = "password";
+    private static final String USER_COLUMN_NAME = "name";
+    private static final String USER_COLUMN_BIRTHDAY = "birthday";
+    private static final String USER_COLUMN_UID = "uid";
+
+    GenshinDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + USER_TABLE_NAME +
+                " (" + USER_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                USER_COLUMN_USERNAME + " TEXT, " +
+                USER_COLUMN_PASSWORD + " TEXT, " +
+                USER_COLUMN_NAME + " TEXT, " +
+                USER_COLUMN_BIRTHDAY + " DATE, " +
+                USER_COLUMN_UID + " TEXT);";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE_NAME);
+        onCreate(db);
+    }
+
+    public Boolean insertUser(String username, String password, String name, Date birthday, String uid) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(USER_COLUMN_USERNAME, username);
+        cv.put(USER_COLUMN_PASSWORD, password);
+        cv.put(USER_COLUMN_NAME, name);
+        cv.put(USER_COLUMN_BIRTHDAY, String.valueOf(birthday));
+        cv.put(USER_COLUMN_UID, uid);
+
+        long result = db.insert(USER_TABLE_NAME, null, cv);
+
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Boolean isUniqueUsername(String username) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE username = ?", new String[]{username});
+
+        if(cursor.getCount() > 0)   // if found out that the username exists, which means USERNAME NOT UNIQUE
+            return true;
+        else                        // if username not taken
+            return false;
+    }
+
+    public Boolean verifyUsernamePassword(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE username = ? AND password = ?", new String[]{username, password});
+
+        if(cursor.getCount() > 0)   // if found out that the both username and password matches
+            return true;
+        else                        // if username and password doesn't match
+            return false;
+    }
 }
--- app/src/main/java/com/mobdeve/group30/genshinbuilds/SignupActivity.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/SignupActivity.java
@@ -2,12 +2,45 @@ package com.mobdeve.group30.genshinbuilds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword, etName, etBirthday, etUid;
+    private Button btnCancel, btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        this.etUsername = findViewById(R.id.et_signup_username);
+        this.etPassword = findViewById(R.id.et_signup_password);
+        this.etName = findViewById(R.id.et_signup_name);
+        this.etBirthday = findViewById(R.id.et_signup_date);
+        this.etUid = findViewById(R.id.et_signup_uid);
+
+        this.btnCancel = findViewById(R.id.btn_signup_cancel);
+        this.btnConfirm = findViewById(R.id.btn_signup_confirm);
+
+        this.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        this.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
