91ac32af0012b1a5a0fc45c51385ca441e667ed5
Ze Long Zhuang <74848471+ZeLongZhuang@users.noreply.github.com>
2021-08-25 20:47:29 +0800

    Update Build.java

--- app/src/main/java/com/mobdeve/group30/genshinbuilds/Build.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/Build.java
@@ -1,4 +1,61 @@
 package com.mobdeve.group30.genshinbuilds;
 
 public class Build {
+    private String character, weapon, artifactSet;
+    int level, hp, atk, def, er, critRate, critDmg;
+
+    public Build(String character, String weapon, String artifactSet, int level, int hp, int atk, int def, int critRate, int crtiDmg) {
+        this.character = character;
+        this.weapon = weapon;
+        this.artifactSet = artifactSet;
+        this.level = level;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.er = er;
+        this.critRate = critRate;
+        this.critDmg = crtiDmg;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public String getArtifactSet() {
+        return artifactSet;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public int getEr() {
+        return er;
+    }
+
+    public int getCritRate() {
+        return critRate;
+    }
+
+    public int getCritDmg() {
+        return critDmg;
+    }
+
+
 }
