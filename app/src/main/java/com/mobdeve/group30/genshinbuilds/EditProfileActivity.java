beeb354446c95214d1274e8f0bd3cab2125b2e7c
Hyacinthium <leana_rebong@dlsu.edu.ph>
2021-09-06 19:16:33 +0800

    added camera integration in EditProfileActivity.java

--- app/src/main/java/com/mobdeve/group30/genshinbuilds/EditProfileActivity.java
+++ app/src/main/java/com/mobdeve/group30/genshinbuilds/EditProfileActivity.java
@@ -1,15 +1,29 @@
 package com.mobdeve.group30.genshinbuilds;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -19,9 +33,17 @@ public class EditProfileActivity extends AppCompatActivity {
 
     Button btnConfirm, btnCancel;
 
+    ImageButton ibCamera; //camera
+
+    ImageView ivProfile;
+
     String currentUsername;
 
     GenshinDatabaseHelper myDB;
+
+    //FOR CAMERA
+    private static int CAMERA_PERMISSION_CODE = 1;
+    private static int CAMERA_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +62,26 @@ public class EditProfileActivity extends AppCompatActivity {
         this.btnConfirm = findViewById(R.id.btn_edit_profile_confirm);
         this.btnCancel = findViewById(R.id.btn_edit_profile_cancel);
 
+        this.ibCamera = findViewById(R.id.ib_edit_profile_camera); //camera
+
+        this.ivProfile = findViewById(R.id.iv_edit_profile_img);
+
         myDB = new GenshinDatabaseHelper(EditProfileActivity.this);
 
         initValues();
+
+        this.ibCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(EditProfileActivity.this, Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_GRANTED){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                }else{
+                    ActivityCompat.requestPermissions((Activity) EditProfileActivity.this, new String[]{Manifest.permission.CAMERA},CAMERA_PERMISSION_CODE);
+                }
+            }
+        });
 
         this.btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +110,29 @@ public class EditProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }else{
+                Toast.makeText(this, "Permission was denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            Bitmap picture = (Bitmap) data.getExtras().get("data");
+            ivProfile.setImageBitmap(picture);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void initValues() {
